4a5eda5cc4d87d94a3d4b48591854714c63214eb
haritshammam <siegkounat@gmail.com>
2021-08-20 11:50:37 +0800

    adding redirect url for vercel url

--- src/constants/spotifyAuthConst.js
+++ src/constants/spotifyAuthConst.js
@@ -1,7 +1,10 @@
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/"
 const SCOPE = "playlist-modify-private playlist-read-private user-read-private user-library-read user-library-modify"
+const REDIRECT_URL_AFTER_LOGIN =
+    process.env.NODE_ENV === 'development' ?
+        "http://localhost:3000/" :
+        "http://utafymusic.vercel.app/"
 
 export {
     CLIENT_ID,
